0a188d07436d3f2d3bfc71089370d09010f82841
Javier Cruz <jcruzsm@gmail.com>
2015-08-31 18:13:13 -0300

    Finish Home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,8 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+
+    assert_select "title", "Home | J+C"
   end
 
 end
